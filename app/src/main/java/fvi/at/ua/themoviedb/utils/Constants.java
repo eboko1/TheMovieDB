f79fba917f8266a67aedd96695e730e352dd9082
Victoria Frantsuh <eboko1@gmail.com>
2017-09-03 13:58:27 +0300

    add Constants.class

---
+++ app/src/main/java/fvi/at/ua/themoviedb/utils/Constants.java
@@ -0,0 +1,13 @@
+package fvi.at.ua.themoviedb.utils;
+
+/**
+ * Created by Vika on 03.09.2017.
+ */
+
+public class Constants {
+
+    public static final class HTTP {
+        public static final String BASE_URL = "https://api.themoviedb.org/";
+
+    }
+}
